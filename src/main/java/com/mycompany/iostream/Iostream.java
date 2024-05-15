eccfd35dfde00574c223bb7e69b28cbae74a77ad
hochausy05 <hochausyhp@gmail.com>
2024-05-15 22:31:41 +0700

    init project

---
+++ src/main/java/com/mycompany/iostream/Iostream.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.iostream;
+
+/**
+ *
+ * @author LEGION
+ */
+public class Iostream {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
